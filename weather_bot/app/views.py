2e9fe7c4e31be9fdd2307a367dd76458461cfcee
Marina <climovicimarina@gmail.com>
2024-10-11 23:46:58 +0200

    project structure

--- weather_bot/app/views.py
+++ weather_bot/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import datetime
+import os
 
-# Create your views here.
+def index(request):
+    API_KEY = os.getenv("API_KEY")
+
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
+    forecast_today_url = "http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
